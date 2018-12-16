8f1f2e164d2a61423c23bc7d9ccbd6236000ec4e
Jakub Merta <mer.kuba@gmail.com>
2018-12-16 12:30:50 +0100

    Beer view model

---
+++ App1/ViewModels/BeerViewModel.cs
@@ -0,0 +1,37 @@
+﻿using App1.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace App1.ViewModels
+{
+    class BeerViewModel : BaseViewModel{
+        public Command LoadItemsCommand { get; set; }
+        public ObservableCollection<Beer> Beers { get; set; }
+
+        public BeerViewModel()
+        {
+            Title = "Beers";
+            Beers = new ObservableCollection<Beer>();
+            LoadItemsCommand = new Command(async () => await ExecuteLoadBeersCommand());
+
+        }
+
+        async Task ExecuteLoadBeersCommand()
+        {
+            if (IsBusy)
+            {
+                return;
+            }
+            IsBusy = true;
+            try
+            {
+                
+            }
+        }
+
+
+    }
+}
