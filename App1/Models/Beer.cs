8d5c8f1b6986a1c499102aa1fe9c23ec8a27ea95
Jakub Merta <mer.kuba@gmail.com>
2018-12-16 11:44:25 +0100

    beer /  model

---
+++ App1/Models/Beer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1.Models
+{
+    class Beer
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+        public string Style { get; set; }
+        public Brewery Brewery { get; set; }
+    }
+}
