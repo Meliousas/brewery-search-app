2344de6e72ff391099d17aa15cebc1754171513f
Jakub Merta <mer.kuba@gmail.com>
2018-12-16 11:44:33 +0100

    brewery / model

---
+++ App1/Models/Brewery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1.Models
+{
+    class Brewery
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+        public List<Beer> BeerList { get; set; }
+    }
+}
