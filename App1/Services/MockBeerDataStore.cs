166a4bd3c995a76450fc98baebe8dabb7070cf99
Jakub Merta <mer.kuba@gmail.com>
2018-12-16 11:52:03 +0100

    mock beer datastore / mocked beer datastore

---
+++ App1/Services/MockBeerDataStore.cs
@@ -0,0 +1,38 @@
+﻿using App1.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1.Services
+{
+    class MockBeerDataStore : IDataStore<Beer>
+    {
+        List<Beer> beers;
+
+        public Task<bool> AddItemAsync(Beer item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteItemAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Beer> GetItemAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Beer>> GetItemsAsync(bool forceRefresh = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateItemAsync(Beer item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
