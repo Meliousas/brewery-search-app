d6da6fdd50aa6e2d9b6941fe911348f798772091
Jakub Merta <mer.kuba@gmail.com>
2018-12-16 11:44:57 +0100

    brewery data store / init

---
+++ App1/Services/BreweryDataStore.cs
@@ -0,0 +1,36 @@
+﻿using App1.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1.Services
+{
+    class BreweryDataStore : IDataStore<Brewery>
+    {
+        public Task<bool> AddItemAsync(Brewery item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteItemAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Brewery> GetItemAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Brewery>> GetItemsAsync(bool forceRefresh = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateItemAsync(Brewery item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
